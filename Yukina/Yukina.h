518c67af591ea3998e8699f0c50acaf617ab4799
Charles Wang <cmw252@cornell.edu>
2015-03-15 01:09:05 -0700

    Initial Commit

---
+++ Yukina/Yukina.h
@@ -0,0 +1,19 @@
+//
+//  Yukina.h
+//  Yukina
+//
+//  Created by Charles Wang on 3/15/15.
+//  Copyright (c) 2015 charlesmwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Yukina.
+FOUNDATION_EXPORT double YukinaVersionNumber;
+
+//! Project version string for Yukina.
+FOUNDATION_EXPORT const unsigned char YukinaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yukina/PublicHeader.h>
+
+
